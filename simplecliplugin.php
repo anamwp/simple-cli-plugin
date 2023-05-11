9e0777a2eb1cb707f1ea117c7c8d3f1f49eb68a4
anam <anam.hossain@multidots.com>
2023-05-11 18:39:26 +0600

    file name updated

--- simplecliplugin.php
+++ simplecliplugin.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name:     Sample CLI plugin
+ * Plugin Name:     Simple CLI plugin
  * Plugin URI:      PLUGIN SITE HERE
  * Description:     PLUGIN DESCRIPTION HERE
  * Author:          Anam
  * Author URI:      https://anam.rocks
- * Text Domain:     samplecliplugin
+ * Text Domain:     simple-cli-plugin
  * Domain Path:     /languages
  * Version:         0.1.0
  *
- * @package         Samplecliplugin
+ * @package         Simplecliplugin
  */
 
  if ( ! defined( 'ABSPATH' ) ) {
@@ -18,12 +18,16 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-final class SAMPLE_CLI_PLUGIN {
+final class SIMPLE_CLI_PLUGIN {
 
 	/**
 	 * plugin version
 	 */
-	const SAMPLE_CLI_PLUGIN_VERSION = '1.0';
+	const SIMPLE_CLI_PLUGIN_VERSION = '1.0';
+	private $level_one;
+	private $level_two;
+	// private $level_one;
+	// private $level_one;
 	/**
 	 * construction of this plugin
 	 */
@@ -31,7 +35,7 @@ final class SAMPLE_CLI_PLUGIN {
 		$this->define_constants();
 		register_activation_hook( __FILE__, array( $this, 'activate' ) );
 		add_action( 'plugins_loaded', array( $this, 'load_plugin_resources' ) );
-		// add_action('cli_init', array( $this, 'anam_cli_register_command' ));
+		add_action('cli_init', array( $this, 'anam_cli_register_command' ));
 	}
 	/**
 	 * Initialize the plugin
@@ -63,29 +67,29 @@ final class SAMPLE_CLI_PLUGIN {
 		/**
 		 * return plugin version
 		 */
-		define( 'SAMPLE_CLI_PLUGIN_VERSION', self::SAMPLE_CLI_PLUGIN_VERSION );
+		define( 'SIMPLE_CLI_PLUGIN_VERSION', self::SIMPLE_CLI_PLUGIN_VERSION );
 		/**
 		 * return the main file name
 		 * C:\xampp\htdocs\devplugin\wp-content\plugins\gutenberg-starter\gutenberg-starter.php
 		 */
-		define( 'SAMPLE_CLI_PLUGIN_FILE', __FILE__ );
+		define( 'SIMPLE_CLI_PLUGIN_FILE', __FILE__ );
 		/**
 		 * return the plugin director
 		 * C:\xampp\htdocs\devplugin\wp-content\plugins\gutenberg-starter
 		 */
-		define( 'SAMPLE_CLI_PLUGIN_PATH', __DIR__ );
+		define( 'SIMPLE_CLI_PLUGIN_PATH', __DIR__ );
 		/**
 		 * return the plugin directory with host
 		 * http://localhost/devplugin/wp-content/plugins/gutenberg-starter
 		 */
-		define( 'SAMPLE_CLI_PLUGIN_URL', plugins_url( '', SAMPLE_CLI_PLUGIN_FILE ) );
-		define( 'SAMPLE_CLI_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
+		define( 'SIMPLE_CLI_PLUGIN_URL', plugins_url( '', SIMPLE_CLI_PLUGIN_FILE ) );
+		define( 'SIMPLE_CLI_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
 		/**
 		 * return the asset folder director
 		 * http://localhost/devplugin/wp-content/plugins/gutenberg-starter/assets
 		 */
-		define( 'SAMPLE_CLI_PLUGIN_ASSETS', SAMPLE_CLI_PLUGIN_URL . '/build' );
-		define( 'SAMPLE_CLI_PLUGIN_DIR_ASSETS', SAMPLE_CLI_PLUGIN_DIR_URL . 'build' );
+		define( 'SIMPLE_CLI_PLUGIN_ASSETS', SIMPLE_CLI_PLUGIN_URL . '/build' );
+		define( 'SIMPLE_CLI_PLUGIN_DIR_ASSETS', SIMPLE_CLI_PLUGIN_DIR_URL . 'build' );
 
 	}
 	/**
@@ -96,10 +100,10 @@ final class SAMPLE_CLI_PLUGIN {
 	 * @return void
 	 */
 	public function activate() {
-		if ( ! get_option( 'sample_cli_plugin_installed' ) ) {
-			update_option( 'sample_cli_plugin_installed', time() );
+		if ( ! get_option( 'simple_cli_plugin_installed' ) ) {
+			update_option( 'simple_cli_plugin_installed', time() );
 		}
-		update_option( 'sample_cli_plugin_version', SAMPLE_CLI_PLUGIN_VERSION );
+		update_option( 'simple_cli_plugin_version', SIMPLE_CLI_PLUGIN_VERSION );
 	}
 	/**
 	 * Load plugin resources
@@ -111,11 +115,15 @@ final class SAMPLE_CLI_PLUGIN {
 		if (!defined('WP_CLI') || !WP_CLI || !class_exists('\WP_CLI')) {
 			return;
 		}
-		$level_one = new Anam\SimpleCLIPlugin\Level_One();
-		\WP_CLI::add_command('l1', $level_one );
-		$level_two = new Anam\SimpleCLIPlugin\Level_Two();
-		\WP_CLI::add_command('l2', $level_two );
+		$this->level_one = new Anam\SimpleCLIPlugin\Level_One();
+		// \WP_CLI::add_command('l1', $level_one );
+		$this->level_two = new Anam\SimpleCLIPlugin\Level_Two();
+		// \WP_CLI::add_command('l2', $level_two );
 		
+	}
+	public function anam_cli_register_command(){
+		\WP_CLI::add_command('l1', $this->level_one );
+		\WP_CLI::add_command('l2', $this->level_two );
 	}
 	
 }
@@ -123,12 +131,12 @@ final class SAMPLE_CLI_PLUGIN {
 /**
  * Initilize the main plugin
  *
- * @return \SAMPLE_CLI_PLUGIN
+ * @return \SIMPLE_CLI_PLUGIN
  */
-function sample_cli_plugin() {
-	 return SAMPLE_CLI_PLUGIN::init();
+function simple_cli_plugin() {
+	 return SIMPLE_CLI_PLUGIN::init();
 }
 /**
  * kick start the plugin
  */
-sample_cli_plugin();
+simple_cli_plugin();
